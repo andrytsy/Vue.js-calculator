d6224ce2af7ef32e1a07c1a26e66d49a7ceba5f1
Tanya Shebankova <andrytsy@mail.ru>
2018-10-04 23:17:09 +0300

    create vue project

---
+++ src/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="calc">
+    TEST
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'claculator'
+}
+</script>
+
+<style>
+#calc {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
